9586aaa73d531fea1590de80aed6c6c8cc4fb68a
si-shakil <sishakil850@gmail.com>
2022-05-23 09:10:38 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Blogs from "./Components/Home/Blogs";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import NotFound from "./Components/Shared/NotFound";
+import Login from "./Components/User/Login";
 import Register from "./Components/User/Register";
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/blogs" element={<Blogs />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <ToastContainer />
