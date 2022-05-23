009ec6185091d06b21c14ab547f7e4692a3cd24c
si-shakil <sishakil850@gmail.com>
2022-05-23 09:19:24 +0600

    navbar updated

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,7 +1,14 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
+  const logOut = () => {
+    signOut(auth);
+  };
   const menu = (
     <>
       <li className="text-2xl">
@@ -13,14 +20,30 @@ const Navbar = () => {
       <li className="text-2xl">
         <Link to="/blogs">Blogs</Link>
       </li>
-      <li className="text-2xl">
-        <Link to="/login">Login</Link>
-      </li>
+      {user && (
+        <li className="text-2xl">
+          <Link to="/dashboard">Dasboard</Link>
+        </li>
+      )}
+      {user && (
+        <li className="text-2xl">
+          <Link to="/my-profile">{user.displayName}</Link>
+        </li>
+      )}
+      {user ? (
+        <li className="text-2xl">
+          <button onClick={logOut}>Sign Out</button>
+        </li>
+      ) : (
+        <li className="text-2xl">
+          <Link to="/login">Login</Link>
+        </li>
+      )}
     </>
   );
   return (
     <div className="navbar bg-gray-700 bg-opacity-20 text-white px-4 lg:px-10">
-      <div className="navbar-start">
+      <div className="navbar-start" style={{ width: "30%" }}>
         <div className="dropdown">
           <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg
@@ -40,7 +63,7 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex="0"
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box"
           >
             {menu}
           </ul>
@@ -52,7 +75,7 @@ const Navbar = () => {
           Xpert
         </Link>
       </div>
-      <div className="navbar-end hidden lg:flex">
+      <div className="navbar-end hidden lg:flex" style={{ width: "70%" }}>
         <ul className="menu menu-horizontal p-0">{menu}</ul>
       </div>
     </div>
