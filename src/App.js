38723bd1d83cc0b6e2e4963bf860e8b0e8237101
si-shakil <sishakil850@gmail.com>
2022-05-23 08:20:02 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LandingPage from "./Components/LandingPage/LandingPage";
+import NotFound from "./Components/Shared/NotFound";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <ToastContainer />
     </div>
